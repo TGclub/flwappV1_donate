016c3e3753fcb5510a6ac5a569bc180477b7dabc
ACERY1 <y841034081@gmail.com>
2018-11-04 14:17:58 +0800

    feat:增加捐赠人名

--- src/pages/donate/donate.js
+++ src/pages/donate/donate.js
@@ -16,10 +16,19 @@ export default class Donate extends React.Component {
       money: evt.target.value
     })
   }
+
+  nameHandler = (evt) => {
+    this.setState({
+      name: evt.target.value
+    })
+  }
+
   render (){
-    const { money } = this.state;
+    const { money, name } = this.state;
     return (
       <div>
+
+        {/* logo区 */}
         <div className="logoWraper">
           <img className="logo-image" src="/static/whale.jpg" alt="logo"/>
           <p className="logo-title">寻找海鲸 共同成长 缔造传统 服务社会</p>
@@ -29,10 +38,19 @@ export default class Donate extends React.Component {
         {/* 分割线 */}
         <p className="splitLine"></p>
 
+        {/* input框 */}
         <div className="donateWraper">
+          <p className="donate-text">捐赠人姓名</p>
+          <input className="donate-input donate-name" type="text"  value={name} onChange={this.nameHandler}/>
+          <p className="donate-text">捐赠金额</p>
           <input className="donate-input" type="number" pattern="\d*" value={money} onChange={this.inputHandler}/>
           <div className="donate-btn">立即捐赠{money ? money + '￥' : ''}</div>
         </div>
+
+        {/* 感谢文案 */}
+        {
+          name && <p className="thanks">{name}，感谢您的捐赠。</p>
+        }
       </div>
     )
   }
