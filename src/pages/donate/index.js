1acaebda941bda0ad0aac41fe7f302a37df82888
ACERY1 <y841034081@gmail.com>
2018-11-29 17:06:54 +0800

    feat:增加openid解析 & 生成海报优化 & 增加弹层

--- src/pages/donate/index.js
+++ src/pages/donate/index.js
@@ -1,37 +1,59 @@
 import './index.less';
 import React from 'react';
 import axios from 'axios';
+import qs from 'qs';
 
+// TODO: 更换服务器URL
+const returnURL = 'http://192.168.31.204:8999/poster.html'
 
-const getPayURL = (openId, amount, returnURL, name) => {
+const getQueryString = (name) => {
+  const paramObj = qs.parse(window.location.search.replace(/\?/, ''));
+  return paramObj[name] ? paramObj[name] : null
+}
+
+const getPayURL = (openId, amount, returnURL, name, phone) => {
+  // oauWk5NsvnCIWGRDOHPc-UOfB9Ek
   // return `http://whalepay.helloyzy.cn/whale/pay/create?openId=${openId}&amount=${amount}&name=%e6%b8%b8%e6%9c%9d%e9%98%b3&phone=18292039753&returnUrl=${returnURL}`
-  return `http://whalepay.helloyzy.cn/whale/pay/create?openId=oauWk5NsvnCIWGRDOHPc-UOfB9Ek&amount=0.01&name=%e6%b8%b8%e6%9c%9d%e9%98%b3&phone=18292039753&returnUrl=http%3a%2f%2f192.168.31.204%3a8999%2fposter.html%3fname%3d${name}`
+  return `http://whalepay.helloyzy.cn/whale/pay/create?openId=${openId}&amount=${amount}&name=${name}&phone=${phone}&returnUrl=${returnURL}?name=${name}`
 }
 
 
 export default class Donate extends React.Component {
   componentDidMount() {
+    // 读URL参数,读出openid, 填入pay的URL
+    const openid = getQueryString('openid')
+    console.log(openid)
+    if (!openid) {
+      alert('捐赠信息校验失败！')
+      window.history.go(-1);
+    }
+    this.setState({
+      openid: openid,
+    })
   }
-  
 
-  constructor () {
+
+  constructor() {
     super();
     this.state = {
       name: '',
       money: '',
+      openid: null,
+      phone: '',
     }
   }
 
   // 捐赠
   donate = () => {
-    if (!this.state.money || typeof parseInt(this.state.money, 10) !== 'number') {
+    if (!this.state.openid || !this.state.phone ||!this.state.name || !this.state.money || typeof parseInt(this.state.money, 10) !== 'number') {
       return false;
     }
     if (!window.wx) {
       return alert('仅支持微信内使用')
     }
-    // TODO:需要加真实入参
-    window.location.href = getPayURL('', '', '', this.state.name);
+    // TODO: 释放金额
+    // window.location.href = getPayURL(this.state.openid, this.state.money, returnURL, this.state.name);
+    window.location.href = getPayURL(this.state.openid, 0.01, returnURL, this.state.name);
   }
 
   inputHandler = (evt) => {
@@ -46,14 +68,20 @@ export default class Donate extends React.Component {
     })
   }
 
-  render (){
-    const { money, name } = this.state;
+  phoneHandler = (evt) => {
+    this.setState({
+      phone: evt.target.value
+    })
+  }
+
+  render() {
+    const { money, name, phone } = this.state;
     return (
       <div>
 
         {/* logo区 */}
         <div className="logoWraper">
-          <img className="logo-image" src="/static/whale.jpg" alt="logo"/>
+          <img className="logo-image" src="/static/whale.jpg" alt="logo" />
           <p className="logo-title">寻找海鲸 共同成长 缔造传统 服务社会</p>
           <p className="logo-subTitle">Service for Whale</p>
         </div>
@@ -64,12 +92,15 @@ export default class Donate extends React.Component {
         {/* input框 */}
         <div className="donateWraper">
           <p className="donate-text">捐赠人姓名</p>
-          <input className="donate-input donate-name" type="text"  value={name} onChange={this.nameHandler}/>
+          <input className="donate-input donate-name" type="text" value={name} onChange={this.nameHandler} />
+          <p className="donate-text">捐赠人电话</p>
+          <input className="donate-input donate-name" type="number" pattern="\d*" value={phone} onChange={this.phoneHandler} />
           <p className="donate-text">捐赠金额</p>
-          <input className="donate-input" type="number" pattern="\d*" value={money} onChange={this.inputHandler}/>
+          <input className="donate-input" type="number" pattern="\d*" value={money} onChange={this.inputHandler} />
           <div className="donate-btn" onClick={this.donate}>立即捐赠{money ? money + '￥' : ''}</div>
         </div>
 
+        <div className="block"></div>
         {/* 感谢文案 */}
         {
           name && <p className="thanks">{name}，感谢您的捐赠。</p>
--- src/pages/poster/index.js
+++ src/pages/poster/index.js
@@ -2,30 +2,59 @@ import './index.less';
 import React from 'react';
 import html2canvas from 'html2canvas';
 import qs from 'qs';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    height: '100%',
+    width: '100%',
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    background: 'rgba(0,0,0,.75)'
+  }
+};
+Modal.setAppElement('#root')
 export default class Poster extends React.Component {
   getQueryString = (name) => {
-    const paramObj = qs.parse(window.location.search.replace(/\?/,''));
+    const paramObj = qs.parse(window.location.search.replace(/\?/, ''));
     return paramObj[name] ? paramObj[name] : null
   }
   componentDidMount() {
     const donateName = this.getQueryString('name');
+    const date = new Date();
     console.log(donateName)
     if (!donateName) {
       alert('捐赠信息校验失败！')
-      window.close();
+      window.history.go(-1);
     }
+    // html2img
     this.setState({
-      name: donateName
-    }, ()=>{
+      name: donateName,
+      year: date.getFullYear(),
+      month: parseInt(date.getMonth(), 10) + 1,
+      day: date.getDate()
+    }, () => {
       setTimeout(() => {
         this.convertCanvasToImage('#target', html2canvas, (canvas) => {
           this.setState({
-            isFinish: true,
             url: canvas.toDataURL(),
+          }, () => {
+            this.setState({
+              isFinish: true,
+            });
           })
         });
-      }, 0)
+      }, 100)
     });
+
+    // close the modal
+    setTimeout(() => {
+      this.closeModal()
+    },2000)
   }
   constructor() {
     super();
@@ -36,7 +65,20 @@ export default class Poster extends React.Component {
       day: '',
       isFinish: false,
       url: '',
+      modalIsOpen: true
     }
+  }
+
+  openModal = () => {
+    this.setState({ modalIsOpen: true });
+  }
+
+  afterOpenModal = () => {
+    // references are now sync'd and can be accessed.
+  }
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
   }
 
   /**
@@ -45,46 +87,58 @@ export default class Poster extends React.Component {
    * @param html2Canvas 库
    * @param cb 回调函数
    */
-  convertCanvasToImage (dom, html2Canvas, cb) {
+  convertCanvasToImage(dom, html2Canvas, cb) {
     const captureContent = document.querySelector(dom);
-      const width = captureContent.offsetWidth;
-      const height = captureContent.offsetHeight;
-      const canvas = document.createElement('canvas');
-      const scale = 2;
-      canvas.height = height * scale;
-      canvas.width  =  width * scale;
-      console.log(canvas.height, canvas.width);
-      const opts = {
-        scale: scale,
-        canvas: canvas,
-        width: width,
-        height: height,
-        useCORS: true,
-      }
-      html2Canvas(captureContent, opts).then(canvas => {
-        const context = canvas.getContext('2d');
-        context.font = "serif";
-        context.mozImageSmoothingEnabled = false;
-        context.webkitImageSmoothingEnabled = false;
-        context.msImageSmoothingEnabled = false;
-        context.imageSmoothingEnabled = false;
-        cb(canvas);
-      });
+    const width = captureContent.offsetWidth;
+    const height = captureContent.offsetHeight;
+    const canvas = document.createElement('canvas');
+    const scale = 2;
+    canvas.height = height * scale;
+    canvas.width = width * scale;
+    console.log(canvas.height, canvas.width);
+    const opts = {
+      scale: scale,
+      canvas: canvas,
+      width: width,
+      height: height,
+      useCORS: true,
+    }
+    html2Canvas(captureContent, opts).then(canvas => {
+      const context = canvas.getContext('2d');
+      context.font = "serif";
+      context.mozImageSmoothingEnabled = false;
+      context.webkitImageSmoothingEnabled = false;
+      context.msImageSmoothingEnabled = false;
+      context.imageSmoothingEnabled = false;
+      cb(canvas);
+    });
   }
 
   render() {
-    const {isFinish, name, year, month, day, url} = this.state
+    const { isFinish, name, year, month, day, url } = this.state
     return (
       <div>
-      {
-        !isFinish ?
-        <div id="target">
-        <img src="/static/post2.jpeg" className="canvas" alt="banner" />
-          <p className="name">{name}</p>
-        </div>
-        :
-        <img src={url} className="pic" alt=""/>
-      }
+        {
+          !isFinish ?
+            <div id="target">
+              <img src="/static/post2.jpeg" className="canvas" alt="banner" />
+              <p className="name">{name}</p>
+              <p className="year">{year}</p>
+              <p className="month">{month}</p>
+              <p className="day">{day}</p>
+            </div>
+            :
+            <img src={url} className="pic" alt="" />
+        }        
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+        <p className="hint">长按保存您的纪念证书</p>
+        </Modal>
       </div>
     )
   }
